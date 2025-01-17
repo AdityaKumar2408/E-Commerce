72dd91ffffe604fcb44d3f5c694113dee59bb75e
Aditya Kumar <110921916+AdityaKumar2408@users.noreply.github.com>
2025-01-17 21:58:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://e-commerce-122.onrender.com',
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
